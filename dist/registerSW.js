dc9d9d218199c4f1d144aaa090f12b0aab28ec50
Pablo Lozano Lominchar <lozanon57@gmail.com>
2026-07-08 01:47:37 +0200

    chore: add .gitignore, remove node_modules and dist from tracking

--- dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/peritoneal-carcinomatosis-app/sw.js', { scope: '/peritoneal-carcinomatosis-app/' })})}
